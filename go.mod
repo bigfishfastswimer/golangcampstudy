7648c6ff35f5a5f8559367761b6217eac15d54fd
Deng Ming <ming_flycash@qq.com>
2023-11-30 12:46:43 +0800

    分布式锁实现

--- go.mod
+++ go.mod
@@ -17,10 +17,11 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/google/uuid v1.3.1
 	github.com/google/wire v0.5.0
+	github.com/gotomicro/redis-lock v0.0.3
 	github.com/hashicorp/golang-lru v0.5.4
 	github.com/lithammer/shortuuid/v4 v4.0.0
 	github.com/prometheus/client_golang v1.17.0
-	github.com/redis/go-redis/v9 v9.2.1
+	github.com/redis/go-redis/v9 v9.3.0
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.17.0
--- webook/ioc/job.go
+++ webook/ioc/job.go
@@ -4,13 +4,14 @@ import (
 	"gitee.com/geekbang/basic-go/webook/internal/job"
 	"gitee.com/geekbang/basic-go/webook/internal/service"
 	"gitee.com/geekbang/basic-go/webook/pkg/logger"
+	rlock "github.com/gotomicro/redis-lock"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/robfig/cron/v3"
 	"time"
 )
 
-func InitRankingJob(svc service.RankingService) *job.RankingJob {
-	return job.NewRankingJob(svc, time.Second*30)
+func InitRankingJob(svc service.RankingService, client *rlock.Client, l logger.LoggerV1) *job.RankingJob {
+	return job.NewRankingJob(svc, l, client, time.Second*30)
 }
 
 func InitJobs(l logger.LoggerV1, rjob *job.RankingJob) *cron.Cron {
@@ -28,7 +29,7 @@ func InitJobs(l logger.LoggerV1, rjob *job.RankingJob) *cron.Cron {
 		},
 	})
 	expr := cron.New(cron.WithSeconds())
-	_, err := expr.AddJob("@every 1m", builder.Build(rjob))
+	_, err := expr.AddJob("@every 1s", builder.Build(rjob))
 	if err != nil {
 		panic(err)
 	}
