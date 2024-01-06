1b819d8d54cf5f6f17ff736bbcfd5ebc4b05effa
Fisher Huang <83265014+fisherhuanganz@users.noreply.github.com>
2024-01-06 16:00:11 +1100

    wire refactor ioc style initialization

--- webook/wire.go
+++ webook/wire.go
@@ -2,6 +2,38 @@
 
 package main
 
-func InitWebookServer() {
+import (
+	"gitee.com/geekbang/basic-go/webook/internal/ioc"
+	"gitee.com/geekbang/basic-go/webook/internal/repository"
+	"gitee.com/geekbang/basic-go/webook/internal/repository/cache"
+	"gitee.com/geekbang/basic-go/webook/internal/repository/dao"
+	"gitee.com/geekbang/basic-go/webook/internal/service"
+	"gitee.com/geekbang/basic-go/webook/internal/web"
+	"github.com/gin-gonic/gin"
+	"github.com/google/wire"
+)
 
+func InitWebookServer() *gin.Engine {
+	wire.Build(
+		// initialize DB
+		ioc.InitDB,
+		// initialize redis
+		ioc.InitRedis,
+		dao.NewUserDAO,
+
+		// initialize cache
+		cache.NewCodeCache, cache.NewUserCache,
+
+		// initialize repository
+		repository.NewCodeRepository,
+		repository.NewUserRepository,
+
+		// service initialization
+		ioc.InitSMSService,
+		service.NewCodeService, service.NewUserService,
+		// handler initialization
+
+		web.NewUserHandler, ioc.InitWebServer, ioc.InitGinMiddlewares,
+	)
+	return gin.Default()
 }
